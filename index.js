168a1eabfafeac15168ec31a084e7967df89b5ec
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-03-28 13:02:18 +0500

    git payment added

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const customer = require("./routes/api/customer");
 const area = require("./routes/api/area");
 const compaign = require("./routes/api/compaign");
 const video = require("./routes/api/video");
+const payment = require("./routes/api/payment");
+
 var busboyBodyParser = require("busboy-body-parser");
+var bodyParser = require("body-parser");
 
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
 const app = express();
+
 connectDB();
 app.use(busboyBodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use("/api/testingAPI", testingAPI);
@@ -23,6 +29,7 @@ app.use("/api/customer", customer);
 app.use("/api/area", area);
 app.use("/api/compaign", compaign);
 app.use("/api/video", video);
+app.use("/api/payment", payment);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/api/payment.js
+++ routes/api/payment.js
@@ -1,4 +1,17 @@
-const { Payment, validate } = require('../../models/Payment');
-const express = require('express');
+const { Payment, validate } = require("../../models/Payment");
+const express = require("express");
 const router = express.Router();
 
+const stripe = require("stripe")("sk_test_tgB9gfnzWwXvw9AGQ5Wwg7Jr00ZQordDJ9");
+
+router.post("/pay", (req, res) => {
+  return stripe.charges
+    .create({
+      amount: req.body.amount, // Unit: cents
+      currency: "eur",
+      description: "Test payment"
+    })
+    .then(result => res.status(200).json(result));
+});
+
+module.exports = router;
